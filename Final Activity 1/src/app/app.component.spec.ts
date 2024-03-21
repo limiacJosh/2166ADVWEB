bf892b0515823d4deeb305e5642c592960fbbaae
Ezekiel <122766315+limiacJosh@users.noreply.github.com>
2024-03-21 16:12:48 +0800

    Add files via upload

---
+++ Final Activity 1/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'FinalsActivity1'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('FinalsActivity1');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FinalsActivity1 app is running!');
+  });
+});
---
+++ Final Activity 1/src/app/service/Book.ts
@@ -0,0 +1,8 @@
+
+export class Book {
+    _id!: String;
+    title!: String;
+    price!: String;
+    description!: String;
+    book_type!: String;
+}
